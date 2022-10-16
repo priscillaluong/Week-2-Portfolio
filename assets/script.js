b2bd4050cead6f7d0b9d8feb74dd9da754afff3a
P Luong <p-luong@live.co.uk>
2022-10-16 17:47:36 +0100

    updated portfolio projects and new nav bar WIP

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// MODAL START
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+// MODAL END
